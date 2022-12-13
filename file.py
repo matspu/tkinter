0063c360922efb5ad820e11ca77549ab1fb34fec
matty.dev <91392614+matspu@users.noreply.github.com>
2022-12-13 20:32:01 +0100

    Update file.py

--- file.py
+++ file.py
@@ -12,7 +12,7 @@ canvas.pack()
 x = 0
 y = 0
 
-tree_size = randint(30, 70)
+tree_size = 100
 present_size = 50
 
 
@@ -26,8 +26,21 @@ def tree_animation():
 
 def player(x, y):
     x = width // 2
-    y = height // 2
-    canvas.create_rectangle(x, y, x+100, y+180, fill="red", tag="player")
+    y = height - 200
+    canvas.create_line(x-30, y-30, x-30, y+200, fill="brown", width="8", tag="player")
+    canvas.create_line(x+120, y-30, x+120, y+200, fill="brown", width="8", tag="player")
+    #lines 
+    canvas.create_line(x-30, y+20, x+120, y+20, fill="brown", width="8", tag="player")
+    canvas.create_line(x-30, y+72, x+120, y+72, fill="brown", width="8", tag="player")
+    canvas.create_line(x-30, y+132, x+120, y+132, fill="brown", width="8", tag="player")
+    
+    canvas.create_rectangle(x, y, x+90, y+160, fill="red", tag="player")
+
+    canvas.create_line(x+10, y+70, x+10, y+150, fill="brown", width="7", tag="player")
+    canvas.create_line(x+7, y+150, x+80, y+150, fill="brown", width="7", tag="player")
+    canvas.create_line(x+80, y+154, x+80, y+70, fill="brown", width="7", tag="player")
+
+    canvas.create_rectangle(x+20, y+90, x+70, y+140, fill="yellow", tag="player")
 
 
 
@@ -36,13 +49,17 @@ def tree(x, y):
     for i in range(5):
         y = 100
         x = randint(0, width-tree_size)
-        canvas.create_oval(x, y, x+tree_size, y+tree_size, fill="green", outline="", tag="tree")
+        canvas.create_oval(x, y, x+tree_size, y+tree_size, fill="#485e52", outline="", tag="tree")
+        canvas.create_oval(x+(tree_size//6), y+(tree_size//6), x+(tree_size*5//6), y+(tree_size*5//6), fill="#587e60", outline="", tag="tree")
+        canvas.create_oval(x+(tree_size//3), y+(tree_size//3), x+(tree_size*2//3), y+(tree_size*2//3), fill="#5f926a", outline="", tag="tree")
         tree_animation()
 
 def present(x, y):
     for i in range(5):
         x = randint(0, width-present_size)
-        canvas.create_rectangle(x, y, x+50, y+50, fill="red", outline="", tag="present")
+        canvas.create_rectangle(x, y, x+75, y+75, fill="#FF3D4E", outline="", tag="present")
+        canvas.create_rectangle(x, y+32, x+75, y+43, fill="white", outline="", tag="present")
+        canvas.create_rectangle(x+32, y+0, x+43, y+75, fill="white", outline="", tag="present")
         present_animation()
 
 
@@ -80,3 +97,7 @@ canvas.bind_all("<Down>", move_down)
 
 
 mainloop()
+
+
+
+
